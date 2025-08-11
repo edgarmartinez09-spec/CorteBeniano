ad8631fbca1569113d3f8afa711a1f902ce0fd80
Jhaseft <jhasesaat@gmail.com>
2025-08-11 19:03:11 -0400

    Avance del proyecto

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,14 @@
+import { Head} from '@inertiajs/react';
+
+export default function Welcome({ auth, laravelVersion, phpVersion }) {
+    
+    return (
+        <>
+            <Head title="Inicio Carrito"/>
+            <div className='text-center text-green-500 text-3xl'>
+                Hola Edgar aca empieza el trabajo
+            </div>
+
+        </>
+    );
+}
